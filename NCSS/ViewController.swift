f2ba6e71c25037ab89354d9752833824a03568db
Edward Poon <edwardpoon1130@gmail.com>
2021-04-17 21:11:58 -0400

    Initial Commit

---
+++ NCSS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NCSS
+//
+//  Created by Edward Poon on 4/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
