5009dc27676b8e27c49ca8a8fddbada91ca3179a
Edward Poon <edwardpoon1130@gmail.com>
2021-04-19 23:06:59 -0400

    Update code

--- NCSS/ShowsViewController.swift
+++ NCSS/ShowsViewController.swift
@@ -12,7 +12,7 @@ class ShowsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        YouTubePlaylistClient(channelId: "UClWTsjnW48j0EIgEyNrdSrg&key=AIzaSyCDGRnmfeJXd6bvFvT8qIm6sDWJtCmHwXA", apiKey: youtubeApiKey).getPlaylists { (networkResult) in
+        YouTubePlaylistClient(channelId: "UClWTsjnW48j0EIgEyNrdSrg", apiKey: youtubeApiKey).getPlaylists { (networkResult) in
             switch(networkResult) {
             case .success(let playlists):
                 print(playlists)
